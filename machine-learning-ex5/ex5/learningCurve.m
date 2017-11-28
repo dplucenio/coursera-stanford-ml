9a5fe610c8dcf091a77efb1052f6435136ab9e25
Daniel Plucenio <dplucenio@gmail.com>
2017-11-28 21:54:32 -0200

    Implement learning curve for lin regression

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -50,7 +50,18 @@ error_val   = zeros(m, 1);
 %           
 %       end
 %
+m_val = size(Xval,1);
+for i = 1:m
+  xx = X(1:i, :);
+  yy = y(1:i, :);
+  theta = trainLinearReg(xx, yy, lambda)
 
+  hh = xx*theta;
+  error_train(i) = 1/(2*i) * (yy-hh)'*(yy-hh);
+
+  h_val = Xval*theta;
+  error_val(i) = 1/(2*m_val) * (yval-h_val)'*(yval-h_val);
+ end
 % ---------------------- Sample Solution ----------------------
 
 
