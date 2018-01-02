fc491e55ee3491ec455f76fec87160b24096b753
Daniel Plucenio <dplucenio@gmail.com>
2018-01-01 23:44:47 -0200

    Implement PCA in week 8 assignment

--- machine-learning-ex7/ex7/pca.m
+++ machine-learning-ex7/ex7/pca.m
@@ -19,7 +19,18 @@ S = zeros(n);
 % Note: When computing the covariance matrix, remember to divide by m (the
 %       number of examples).
 %
-
+cov = 1/m * X'*X;
+% since cov is symmetrical (provides n orthornormal eigenvector), and positive
+% definite if columns are lineraly independent, svd reduces to an eigenvalue
+% problem, since the same basis formed by the eigenvalues will be orthonormal
+% basis for the row and column space. SVD would just carry extra computation 
+% which is really valuable in cases where matrices are no squared or nullspaces
+% or even aren't diagonalizable. But no need for a matrix like the covariance
+% matrix. But svd is called here because coursera grading system is using it
+% to validate results and the eigenvectors found by eig function is on opposite
+% direction, which is completely valid but the coursera assertion is not
+% prepared for that
+[U S V] = svd(cov);
 
 
 
